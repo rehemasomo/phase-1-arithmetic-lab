c31b73ab22b3cf7d921259dc7b84ca630b634003
rehemasomo <rehemasomo30@gmail.com>
2024-03-22 14:37:45 +0300

    completed arithmetic lab

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
-
 // Write your code here
+let num1 = 31;
+let num2 = 62 / num1 ;
+let multiply = num1 * num2;
+
+let random = Math.floor(Math.random() * Number.MAX_SAFE_INTEGER) + 1;
+let output = random > 0;
+console.log(output); // Expected result: true
+
+
+let num3 = 20; // define num3 
+let num4 = 8;
+let mod = num3 % num4;
+console.log(mod); //expected result:4
+
+
+let max= 20;
+console.log(max); // Expected result: 20
+console.log(max === 20); //Expected result: true
